dd40dc3000ec03bfc528e266a41f260f621675f2
TonyMar <1161602026@qq.com>
2017-12-31 09:10:41 +0800

    rename the uid and iid

--- surprise/prediction_algorithms/predictions.py
+++ surprise/prediction_algorithms/predictions.py
@@ -21,14 +21,14 @@ class PredictionImpossible(Exception):
 
 
 class Prediction(namedtuple('Prediction',
-                            ['uid', 'iid', 'r_ui', 'est', 'details'])):
+                            ['urid', 'irid', 'r_ui', 'est', 'details'])):
     """A named tuple for storing the results of a prediction.
 
     It's wrapped in a class, but only for documentation and printing purposes.
 
     Args:
-        uid: The (raw) user id. See :ref:`this note<raw_inner_note>`.
-        iid: The (raw) item id. See :ref:`this note<raw_inner_note>`.
+        urid: The (raw) user id. See :ref:`this note<raw_inner_note>`.
+        irid: The (raw) item id. See :ref:`this note<raw_inner_note>`.
         r_ui(float): The true rating :math:`r_{ui}`.
         est(float): The estimated rating :math:`\\hat{r}_{ui}`.
         details (dict): Stores additional details about the prediction that
@@ -38,8 +38,8 @@ class Prediction(namedtuple('Prediction',
     __slots__ = ()  # for memory saving purpose.
 
     def __str__(self):
-        s = 'user: {uid:<10} '.format(uid=self.uid)
-        s += 'item: {iid:<10} '.format(iid=self.iid)
+        s = 'user: {uid:<10} '.format(uid=self.urid)
+        s += 'item: {iid:<10} '.format(iid=self.irid)
         if self.r_ui is not None:
             s += 'r_ui = {r_ui:1.2f}   '.format(r_ui=self.r_ui)
         else:
